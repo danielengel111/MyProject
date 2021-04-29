0e801f98a02e5a40155eecab5148bfbe8c859a8d
StasEngel <69434834+StasEngel@users.noreply.github.com>
2021-04-29 19:14:22 +0300

    class Secret

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,6 +1,18 @@
-public class MyClass {
-    public static void main(String[] args) {
-        int y = 7;
+import java.lang.invoke.ConstantCallSite;
+import java.util.Scanner;
+
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        //Scanner scanner = new Scanner(System.in);
+
+        Secret s1 = Secret.getNewSecret();
+        Secret s2 = Secret.getNewSecret();
+        Secret s3 = Secret.getNewSecret();
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
         int x = 8;
         int z = 9;
         int bobo = 10;
